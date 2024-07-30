5b0c3afded9444585d90ec3d68ef210366d1fc8f
imran <imran@imrans-MacBook-Air.local>
2024-07-30 19:29:01 +0530

    get following users endpoint

--- src/main/java/com/example/userservice/repository/UserRepository.java
+++ src/main/java/com/example/userservice/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,4 +18,7 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
     @Query(value = "SELECT u, f FROM User u LEFT JOIN Follow f ON u.id = f.following_id")
     Page<?> findAllWithFollowers(Pageable pageable);
+
+    @Query(value = "SELECT u FROM User u WHERE u.id IN (SELECT f.following_id FROM Follow f WHERE f.follower_id = :userId)")
+    List<?> findFollowingByUserId(@Param("userId") UUID userId);
 }
